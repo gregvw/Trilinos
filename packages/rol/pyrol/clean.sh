d1e5f08f782a33984b205da1f195ab40611ab8a1
gregvw <gvonwin@sandia.gov>
2017-04-19 08:30:24 -0600

    updated clean to preserve all .sh files in build

--- packages/rol/pyrol/clean.sh
+++ packages/rol/pyrol/clean.sh
@@ -4,13 +4,14 @@ cwd=`pwd`
 cwd=`basename $cwd`
 if [ $cwd == "build" ]; then 
   mkdir temp
-  mv "do-configure-pyrol.sh" temp
-  mv "clean.sh" temp
+  cp *.sh temp
   ls | grep -v temp | xargs rm -rf
-  mv temp/* ./
+  cp temp/* ./
   rm -rf temp
-  chmod u+x "do-configure-pyrol.sh"
-  chmod u+x "clean.sh"
+
+  for filename in *.sh; do
+    chmod u+x $filename
+  done
 else
   echo "clean can only be run from the build directory"
 fi
